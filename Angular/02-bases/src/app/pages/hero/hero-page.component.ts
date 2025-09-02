f73c25f84f86312f17751a717d46855dad2c3e2a
leticia.orive <leticia.orive@accenture.com>
2025-09-02 08:36:12 +0200

    señales computadas-Readonly Signals

--- Angular/02-bases/src/app/pages/hero/hero-page.component.ts
+++ Angular/02-bases/src/app/pages/hero/hero-page.component.ts
@@ -3,19 +3,19 @@ import { UpperCasePipe } from '@angular/common';
 
 @Component({
   templateUrl: './hero-page.component.html',
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe]
+
 })
 export class HeroPageComponent {
   name = signal('Ironman');
   age = signal(45);
 
-  heroDescription = computed(() => {
+    heroDescription = computed(() => {
     const description = `${this.name()} - ${this.age()}`;
     return description;
   });
 
   capitalizedName = computed(() => this.name().toUpperCase());
-
   changeHero() {
     this.name.set('Spiderman');
     this.age.set(22);
