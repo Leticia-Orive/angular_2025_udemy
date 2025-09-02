772dd69c7be1fb436a6727ae442d3755bd991779
leticia.orive <leticia.orive@accenture.com>
2025-09-02 12:43:02 +0200

    solucion a la tarea

--- Angular/02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ Angular/02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -17,9 +17,12 @@ interface Character {
 
 })
 export class DragonballComponent {
+
   //Creamos dos señales independientes
   name = signal('Gohan');
   power = signal(100);
+
+
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
@@ -36,5 +39,31 @@ export class DragonballComponent {
 
     }
   })*/
+ addCharacter() {
+  //añadir un personaje vamos hacer distintas validaciones
+  if(!this.name() || !this.power() || this.power() <= 0) {
 
+    return;
+  }
+  const newCharacter: Character = {
+    id: this.characters().length + 1,
+    name: this.name(),
+    power: this.power()
+  };
+  //esto es una forma pero no le gusta mucho a la gente
+  //this.characters().push(newCharacter);
+
+  //lo que se recomienda:
+  this.characters.update(list => [...list, newCharacter]);
+  //el resetFields lo voy a mandar llamar tan pronto se inserte en nuestro arreglo
+  this.resetFields();
+ }
+
+ //para pulgarlo hariamos un nuevo metodo
+ resetFields(){
+  this.name.set('');
+  this.power.set(0);
+ }
 }
+
+
