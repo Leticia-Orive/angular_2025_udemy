a5f90dfff93c7b1bda5fa8f28383eef8614fa7eb
leticia.orive <leticia.orive@accenture.com>
2025-09-02 10:34:42 +0200

    preparacion de la pagina

--- Angular/02-bases/src/app/app.routes.ts
+++ Angular/02-bases/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballComponent } from './pages/dragonball/dragonball-page.component';
+
 
 
 export const routes: Routes = [
@@ -13,9 +15,16 @@ export const routes: Routes = [
     path: 'hero',
     component: HeroPageComponent
   },
+
+  {
+    path: 'dragonball',
+    component: DragonballComponent
+  },
+
   //redirect vacio
   {
     path: '**',
     redirectTo: ''
-  }
+  },
+
 ];
