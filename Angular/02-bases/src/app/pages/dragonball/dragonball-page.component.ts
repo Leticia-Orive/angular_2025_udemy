252ef5a771166dc60ad55d0354c87ae6f0fb3299
leticia.orive <leticia.orive@accenture.com>
2025-09-02 11:10:48 +0200

    ngClass

--- Angular/02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ Angular/02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from "@angular/core";
+import { NgClass } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 
 interface Character {
@@ -10,15 +11,26 @@ interface Character {
 @Component({
   selector: 'app-dragonball',
   templateUrl: './dragonball-page.component.html',
+  imports: [
+    //NgClass
+  ],
 
 })
 export class DragonballComponent {
 
   characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 15000 },
-    { id: 2, name: 'Vegeta', power: 14000 },
-    { id: 3, name: 'Gohan', power: 13000 },
-    { id: 4, name: 'Trunks', power: 12000 },
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Gohan', power: 4000 },
+    { id: 4, name: 'Trunks', power: 3000 },
   ])
+//Series de clases
+//Tambien se puede hacer con un signal
+/**  powerClasses = computed (() => {
+    return {
+      'text-danger': true
+
+    }
+  })*/
 
 }
