dab6df989451b7aee97530262be726ea5cd55ade
leticia.orive <leticia.orive@accenture.com>
2025-09-02 09:39:31 +0200

    routerLink

---
+++ Angular/02-bases/src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+
+})
+export class Navbar {
+
+}
+//Routerlink es una directiva que sirven para cambiar la funcionamiento de algo
