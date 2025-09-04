81e298ddd5a738468934063ca5cdb798e009e2f1
leticia.orive <leticia.orive@accenture.com>
2025-09-04 08:22:54 +0200

    routerLinks desde componente hijo

--- Angular/03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/gifs-side-menu-options.component.ts
+++ Angular/03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/gifs-side-menu-options.component.ts
@@ -1,11 +1,34 @@
 import {Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 
 })
-export default class GifsSideMenuOptionsComponent { }
+export default class GifsSideMenuOptionsComponent {
+
+  //nueva propiedad
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    },
+  ];
+}
