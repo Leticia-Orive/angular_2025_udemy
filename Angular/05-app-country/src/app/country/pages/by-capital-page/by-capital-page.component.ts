1fdd6f88baaaa871598e1147a4d6bb044e4b1061
leticia.orive <leticia.orive@accenture.com>
2025-09-11 13:06:08 +0200

    inputs y tablas

--- Angular/05-app-country/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ Angular/05-app-country/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './by-capital-page.component.html',
 })
-export class ByCapitalPageComponent { }
+export class ByCapitalPageComponent {
+
+  //Creamos un metodo internamente
+
+  onSearch(value: string) {
+    console.log(value);
+  }
+ }
