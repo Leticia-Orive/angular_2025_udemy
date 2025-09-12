d065f4973971a6bf79bffe71cc831012b0645ad8
leticia.orive <leticia.orive@accenture.com>
2025-09-12 09:21:30 +0200

    countryServices interfaces

--- Angular/05-app-country/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ Angular/05-app-country/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { count } from 'rxjs';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,10 +10,16 @@ import { CountryListComponent } from '../../components/country-list/country-list
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+  //Injectamos el servicio
+  CountryService = inject(CountryService);
+
 
   //Creamos un metodo internamente
 
-  onSearch(value: string) {
-    console.log(value);
+  onSearch(query: string) {
+    this.CountryService.searchByCapital(query)
+      .subscribe(resp => {
+        console.log(resp);
+      } );
   }
  }
