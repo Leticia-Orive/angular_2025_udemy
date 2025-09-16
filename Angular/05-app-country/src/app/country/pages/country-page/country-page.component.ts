1b661cfd082b4785666ea7fadbd42c178a6e9fb5
leticia.orive <leticia.orive@accenture.com>
2025-09-16 12:20:51 +0200

    informacion de un pais

--- Angular/05-app-country/src/app/country/pages/country-page/country-page.component.ts
+++ Angular/05-app-country/src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
   imports: [],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+  //esta son dos formas de obtener el parametro
+  //countryCode = inject(ActivatedRoute).snapshot.paramMap.get('code');
+  countryCode = inject(ActivatedRoute).snapshot.params['code'];
+  countryService = inject(CountryService);
+
+  countryResource = rxResource({
+    request: () => ({code: this.countryCode}),
+    loader: ({ request }) => {
+      return this.countryService.searchCountryByAlphaCode(request.code);
+    }
+  })
+}
