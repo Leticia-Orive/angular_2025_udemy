8282051bb18226879b0f0594de4ec650f63acb7d
leticia.orive <leticia.orive@accenture.com>
2025-09-17 13:41:04 +0200

    decimal, percent y currency pipes

--- Angular/04-pipes-app/src/app/pages/numbers-page/numbers-page.component.ts
+++ Angular/04-pipes-app/src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DecimalPipe, PercentPipe, CurrencyPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.component.html',
 
 })
-export default class NumbersPageComponent { }
+export default class NumbersPageComponent {
+  totalSells = signal(2_433_232.5567);
+  percent = signal(0.4856);
+}
