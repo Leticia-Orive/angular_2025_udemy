cd7103ddeced4be0a3311b007cb689d08de166e8
leticia.orive <leticia.orive@accenture.com>
2025-09-17 10:08:17 +0200

    inicio proyecto pipeApp

---
+++ Angular/04-pipes-app/src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes Basicos',
+    loadComponent: () => import('./pages/basic-page/basic-page.component'),
+  },
+  {
+    path: 'numbers',
+    title: 'Pipes Numericos',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes Inusuales',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page.component'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes Personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  }
+];
---
+++ Angular/04-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uncommon-page',
+  imports: [],
+  templateUrl: './uncommon-page.component.html',
+
+})
+export default class UncommonPageComponent { }
