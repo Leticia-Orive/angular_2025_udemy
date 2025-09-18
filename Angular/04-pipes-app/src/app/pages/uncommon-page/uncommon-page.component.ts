05d11be4b6c68e423341f7f40828620735c9b834
leticia.orive <leticia.orive@accenture.com>
2025-09-18 13:41:42 +0200

    asyncPipe

--- Angular/04-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ Angular/04-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,18 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import {
-   I18nPluralPipe,
-   I18nSelectPipe,
-   JsonPipe,
-   KeyValuePipe,
-   SlicePipe,
-   TitleCasePipe,
-   UpperCasePipe
-  } from '@angular/common';
+  AsyncPipe,
+  I18nPluralPipe,
+  I18nSelectPipe,
+  JsonPipe,
+  KeyValuePipe,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
 
 //Me creo dos clientes
-const client1 = { name: 'Leticia', gender: 'female', age: 31, address: 'Calle Luna Calle Sol' };
-const client2 = { name: 'Cristian', gender: 'male', age: 37, address: 'Calle Sol Calle Luna' };
+const client1 = {
+  name: 'Leticia',
+  gender: 'female',
+  age: 31,
+  address: 'Calle Luna Calle Sol',
+};
+const client2 = {
+  name: 'Cristian',
+  gender: 'male',
+  age: 37,
+  address: 'Calle Sol Calle Luna',
+};
 
 @Component({
   selector: 'app-uncommon-page',
@@ -24,13 +35,12 @@ const client2 = { name: 'Cristian', gender: 'male', age: 37, address: 'Calle Sol
     JsonPipe,
     UpperCasePipe,
     KeyValuePipe,
-    TitleCasePipe
+    TitleCasePipe,
+    AsyncPipe,
   ],
   templateUrl: './uncommon-page.component.html',
-
 })
 export default class UncommonPageComponent {
-
   //Creo una señal
   //i18n Select
   client = signal(client1);
@@ -51,30 +61,28 @@ export default class UncommonPageComponent {
 
   //i18n Plural
   //otro objecto literal
-  clientsMap = signal ({
+  clientsMap = signal({
     '=0': 'no tenemos ningún cliente',
     '=1': 'tenemos un cliente',
     '=2': 'tenemos dos clientes',
-    'other': 'tenemos # clientes'
-  })
-  clients = signal(
-    [
-      'Leticia',
-      'Cristian',
-      'Ana',
-      'Pepe',
-      'Maria',
-      'Juan',
-       'Laura',
-       'Alberto',
-       'Sofia',
-       'Alvaro',
-       'Marta'
-    ]
-  );
+    other: 'tenemos # clientes',
+  });
+  clients = signal([
+    'Leticia',
+    'Cristian',
+    'Ana',
+    'Pepe',
+    'Maria',
+    'Juan',
+    'Laura',
+    'Alberto',
+    'Sofia',
+    'Alvaro',
+    'Marta',
+  ]);
 
   deleteClient() {
-    this.clients.update(prev => prev.slice(1));
+    this.clients.update((prev) => prev.slice(1));
   }
 
   //Nueva entrada KeyValue Pipe
@@ -84,8 +92,14 @@ export default class UncommonPageComponent {
     name: 'Leticia',
     age: 31,
     address: 'Calle Luna Calle Sol',
+  };
 
-
-  }
-
+  //Async Pipe para que funcione necesito algo asincrono trabaja con observables y promesas
+  promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('Tenemos un error en la data');
+      //resolve('Tenemos data en la promesa');
+      console.log('Tenemos data en la promesa');
+    }, 3500);
+  });
 }
