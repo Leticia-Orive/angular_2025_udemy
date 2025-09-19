b23c62e33d17eba0ae7a667788eca0a3c5d0fbbb
leticia.orive <leticia.orive@accenture.com>
2025-09-19 14:04:30 +0200

    CanFlyPipe , HeroColor, HeroTextColor

--- Angular/04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ Angular/04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,10 +1,21 @@
 import {  Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
+import { CanFyPipe } from "../../pipes/can-fly.pipe";
+import { HeroColorPipe } from "../../pipes/heroColor.pipe";
+import { HeroTextColorPipe } from "../../pipes/hero-text-color.pipe";
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe],
+  imports: [
+    ToggleCasePipe,
+    CanFyPipe,
+    HeroColorPipe,
+    HeroTextColorPipe,
+    TitleCasePipe,
+  HeroCreatorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
