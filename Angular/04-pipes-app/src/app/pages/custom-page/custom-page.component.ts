3f1413d976cc2367cb84f25c2711ba613340d9f7
leticia.orive <leticia.orive@accenture.com>
2025-09-22 10:44:33 +0200

    heroFilterPipe

--- Angular/04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ Angular/04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -8,6 +8,7 @@ import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 import { HeroSortByPipe } from "../../pipes/hero-sort-by.pipe";
 import { Hero } from '../../interfaces/hero.interface';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
@@ -18,7 +19,8 @@ import { Hero } from '../../interfaces/hero.interface';
     HeroTextColorPipe,
     TitleCasePipe,
     HeroCreatorPipe,
-    HeroSortByPipe
+    HeroSortByPipe,
+    HeroFilterPipe
 ],
   templateUrl: './custom-page.component.html',
 })
@@ -32,4 +34,5 @@ export default class CustomPageComponent {
 
   //Crearme una nueva señal
   sortBy = signal<keyof Hero | null>(null)
+  searchQuery = signal('');
 }
