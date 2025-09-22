981143b4ec0fcafd21e19a75abd1e980b265ce76
leticia.orive <leticia.orive@accenture.com>
2025-09-22 12:55:40 +0200

    Formulario app

---
+++ Angular/06-reactive-forms-app/src/app/country/pages/country.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from "@angular/router";
+import { CountryPageComponent } from "./country-page/country-page.component";
+
+
+
+export const authRoutes: Routes = [
+  {
+    path: '',
+    component: CountryPageComponent
+  },
+]
---
+++ Angular/06-reactive-forms-app/src/app/reactive/pages/reactive.routes.ts
@@ -0,0 +1,35 @@
+import { Routes } from "@angular/router";
+import { BasicPageComponent } from "./basic-page/basic-page.component";
+import { DynamicPageComponent } from "./dynamic-page/dynamic-page.component";
+import { SwitchesPageComponent } from "./switches-page/switches-page.component";
+
+
+
+export const authRoutes: Routes = [
+  {
+    path: '',
+    children:[
+      {
+        path: 'basic',
+        title: 'Basicos',
+        component: BasicPageComponent
+      },
+      {
+        path: 'dynamic',
+        title: 'Dinamicos',
+        component: DynamicPageComponent
+      },
+      {
+        path: 'switches',
+        title: 'Switches',
+        component: SwitchesPageComponent
+      },
+
+        {
+        path: '**',
+        redirectTo: 'basic'
+      },
+
+    ]
+  },
+]
