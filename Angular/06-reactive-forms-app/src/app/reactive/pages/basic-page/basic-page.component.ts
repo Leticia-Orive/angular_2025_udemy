390b6465986a71f7a79bd299aab028553e0bd573
leticia.orive <leticia.orive@accenture.com>
2025-09-24 14:01:42 +0200

    Mostrar errores en pantalla

--- Angular/06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ Angular/06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { JsonPipe } from '@angular/common';
+import { ReturnStatement } from '@angular/compiler';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
@@ -15,7 +16,7 @@ export class BasicPageComponent {
   //fb = FormBuilder; --- IGNORE ---
   private fb = inject(FormBuilder);
 
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     //Definimos nuestro formulario con formControl
     name: ['', [Validators.required, Validators.minLength(3)]  /** Validadores sincronos */ ,/**Validadores asincronos[] */],
     price: [0, [Validators.required, Validators.min(10)]],
@@ -36,5 +37,30 @@ export class BasicPageComponent {
     inStorage: new FormControl(0),
 
   })*/
+
+    //me creo una funcion
+    isValidField(fieldName: string): boolean | null {
+      return !! this.myForm.controls[fieldName].errors //le paso por la doble negacion !! para que
+      //primera mente este vacio o el valor opuesto a vacio es que tenga algo
+    }
+
+    getFieldError(fieldName: string): string | null {
+      if (!this.myForm.controls[fieldName]) return null;
+      const errors = this.myForm.controls[fieldName].errors ?? {};
+      for (const key of Object.keys(errors)) {
+        switch (key) {
+          case 'required':
+            return 'Este campo es obligatorio';
+          case 'minlength':
+            return `Mínimo ${errors['minlength'].requiredLength} caracteres`;
+          case 'min':
+            return `El valor mínimo es ${errors['min'].min}`;
+
+
+        }
+      }
+      return null;
+
+    }
 
  }
