2fe28d51d5915776b2cb348031bd6620a72eab5d
leticia.orive <leticia.orive@accenture.com>
2025-09-24 09:07:36 +0200

    formGroup, fromControl

--- Angular/06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ Angular/06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,21 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+
+  //Para que funcione un formulario reactivo
+
+  myForm = new FormGroup({
+    //Definimos nuestro formulario
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  })
+
+ }
