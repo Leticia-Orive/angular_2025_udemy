a466dce83c6672fa37481c19115c77e37103288c
leticia.orive <leticia.orive@accenture.com>
2025-09-25 08:08:20 +0200

    formulario dinamico con arreglos

--- Angular/06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ Angular/06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,29 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators,  FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
+
+  //me creo el formulario
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ],
+    //para que por lo menos tenga dos juegos o elementos
+    Validators.minLength(2)
+  )
+  });
+  //me creo un getter para los favoritos y me permite invocar y regresar lo que necesito
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+}
