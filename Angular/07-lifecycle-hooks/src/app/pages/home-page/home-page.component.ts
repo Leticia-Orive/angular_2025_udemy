f560b45cd5079b37781d1447c7d2f44766b44998
leticia.orive <leticia.orive@accenture.com>
2025-09-29 17:13:29 +0200

    ciclo de vida de los componentes

--- Angular/07-lifecycle-hooks/src/app/pages/home-page/home-page.component.ts
+++ Angular/07-lifecycle-hooks/src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { afterNextRender, afterRender, Component } from '@angular/core';
 
 
-
+const log = (...messages: string[]) => {
+  console.log(
+    `${messages[0]} %c${messages.slice(1).join(', ')} `,
+    'color: #bada55'
+  );
+};
 
 @Component({
   selector: 'app-home-page',
@@ -9,6 +14,69 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent  {
+  constructor(){
+    console.log('constructor llamado')
+  }
+  ngOnInit() {
+    log(
+      'ngOnInit',
+      "Runs once after Angular has initialized all the component's inputs."
+    );
+  }
+
+  ngOnChanges() {
+    log('ngOnChanges', "Runs every time the component's inputs have changed.");
+  }
+
+  ngDoCheck() {
+    log('ngDoCheck', 'Runs every time this component is checked for changes.');
+  }
+
+  ngAfterContentInit() {
+    log(
+      'ngAfterContentInit',
+      "Runs once after the component's content has been initialized."
+    );
+  }
+
+  ngAfterContentChecked() {
+    log(
+      'ngAfterContentChecked',
+      'Runs every time this component content has been checked for changes.'
+    );
+  }
+
+  ngAfterViewInit() {
+    log(
+      'ngAfterViewInit',
+      "Runs once after the component's view has been initialized."
+    );
+  }
+
+  ngAfterViewChecked() {
+    log(
+      'ngAfterViewChecked',
+      "Runs every time the component's view has been checked for changes."
+    );
+  }
+
+  ngOnDestroy() {
+    log('ngOnDestroy', '	Runs once before the component is destroyed.');
+  }
+
+  afterNextRenderEffect = afterNextRender(() => {
+    log(
+      'afterNextRender',
+      'Runs once the next time that all components have been rendered to the DOM.'
+    );
+  });
+
+  afterRender = afterRender(() => {
+    log(
+      'afterRender',
+      'Runs every time all components have been rendered to the DOM.'
+    );
+  });
 
   }
 
