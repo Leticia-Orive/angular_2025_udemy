f8ece67ef4a01845c981be3230e746cdbcf1f0a1
leticia.orive <leticia.orive@accenture.com>
2025-10-02 08:39:36 +0200

    rutas y componentes

--- Angular/08-maps-app/src/app/app.component.ts
+++ Angular/08-maps-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
