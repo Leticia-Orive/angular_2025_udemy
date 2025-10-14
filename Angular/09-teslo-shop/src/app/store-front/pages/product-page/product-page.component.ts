209f2e584a6a74d1ca307ba5b7f78530730886d2
leticia.orive <leticia.orive@accenture.com>
2025-10-14 16:28:34 +0200

    Carousel de imagenes

--- Angular/09-teslo-shop/src/app/store-front/pages/product-page/product-page.component.ts
+++ Angular/09-teslo-shop/src/app/store-front/pages/product-page/product-page.component.ts
@@ -2,9 +2,10 @@ import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '@products/services/products.service';
+import { ProductCarouselComponent } from "@products/components/product-carousel/product-carousel.component";
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCarouselComponent, ProductCarouselComponent],
   templateUrl: './product-page.component.html',
 })
 export class ProductPageComponent {
